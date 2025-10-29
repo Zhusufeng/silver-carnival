4bd97acf0e58835fff3596afa438879d23f20907
Lisa Gee <zhusufeng@gmail.com>
2025-10-29 08:53:05 -0700

    Add custom Node Express server

---
+++ server.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("/api/hello", (_req: Request, res: Response) => {
+    res.json({ message: "Hello from custom Node.js server!" });
+  });
+
+  // This is to serve the frontend pages
+  server.all(/.*/, (req: Request, res: Response) => {
+    return handle(req, res);
+  });
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
